e89aa1159efea8cc031a94e24197b5e5d63b4e45
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-10-29 22:06:07 +0200

    Update vector.h

--- vector.h
+++ vector.h
@@ -13,18 +13,20 @@ using std::chrono::high_resolution_clock;
 using std::chrono::microseconds;
 using std::chrono::seconds;
 using namespace std;
+
 struct Studentas {
   string vardas;
   string pavarde;
-  vector<int> nd;
-  int egz; 
-  double galutinis;
+  vector<int> nd; 
+  int egz;
+  double galutinis; 
 };
 
-vector<Studentas> nuskaityti_duomenis(const string& failo_vardas);
 
-pair<vector<Studentas>, vector<Studentas>> padalinti_studentus(const vector<Studentas>& studentai);
+vector<Studentas> nuskaityti_duomenis_vector(const string& failo_vardas);
 
-void surusiuoti_studentus(vector<Studentas>& studentai);
+pair<vector<Studentas>, vector<Studentas>> padalinti_studentus_vector(const vector<Studentas>& studentai);
 
-void isvesti_duomenis(const string& failo_vardas, const vector<Studentas>& studentai);
+void surusiuoti_studentus_vector(vector<Studentas>& studentai);
+
+void isvesti_duomenis_vector(const string& failo_vardas, const vector<Studentas>& studentai);
