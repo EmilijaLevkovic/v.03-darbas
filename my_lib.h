0483f2ffd249e39d79f5142720395fece580c5eb
EmilijaLevkovic <145105325+EmilijaLevkovic@users.noreply.github.com>
2023-10-29 19:43:13 +0200

    Update my_lib.h

--- my_lib.h
+++ my_lib.h
@@ -6,16 +6,25 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+using std::vector;
+using std::string;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 using std::chrono::microseconds;
 using std::chrono::seconds;
 using namespace std;
-
 struct Studentas {
   string vardas;
   string pavarde;
-  vector<int> nd; 
+  vector<int> nd;
   int egz; 
-  double galutinis; 
+  double galutinis;
 };
+
+vector<Studentas> nuskaityti_duomenis(const string& failo_vardas);
+
+pair<vector<Studentas>, vector<Studentas>> padalinti_studentus(const vector<Studentas>& studentai);
+
+void surusiuoti_studentus(vector<Studentas>& studentai);
+
+void isvesti_duomenis(const string& failo_vardas, const vector<Studentas>& studentai);
